a1e090428d1d57485b72e39fff78281ba4686809
AlexCondor <alexguerron@gmail.com>
2023-12-13 19:46:22 -0500

    Vue: Directivas V-model V-for

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 console.log('elementos vue');
 console.log(Vue);
+const estudiantes=[
+    {nombre:'a',edad:18},
+    {nombre:'b',edad:24},
+    {nombre:'c',edad:30},
+    {nombre:'d',edad:32},
+    {nombre:'e',edad:20},
+]
+console.log(estudiantes);
+console.table(estudiantes);
 
 const app=Vue.createApp({
     /*
@@ -12,6 +21,15 @@ const app=Vue.createApp({
 
     */
     //-----------Options API-----------
+    data(){
+        return{
+            mensaje:'hola mundo desde Vue.js',
+            valor:100,
+            lista:estudiantes,
+            nombre:null,
+            edad:null
+        }
+    },
     methods:{
         cambiarMensaje(){
             console.log('entro a cambiar mensaje');
@@ -20,16 +38,18 @@ const app=Vue.createApp({
         },
         sumar(){
             this.valor++
+        },
+        agregar(){
+            this.lista.push({nombre:this.nombre, edad:this.edad})
+        },
+        pesionandotecla(event){
+            console.log('click');
+            console.log(event.charCode);
+            if(event.charCode===13){
+                this.agregar();
+            }
         }
     },
-    watch:{
-
-    },
-    data(){
-        return{
-            mensaje:'hola mundo desde Vue.js',
-            valor:100
-        }
-    }
+    watch:{}
 });
 app.mount('#myApp')
